8e7499a73e9d4ea2dad031a95a1aa6259dabb974
ravinder sian <rsian761@gmail.com>
2022-07-10 20:51:20 +0100

    add auth

--- hotel-project-auth/src/main/java/com/personal/hotel/controller/SlotMachineController.java
+++ hotel-project-auth/src/main/java/com/personal/hotel/controller/SlotMachineController.java
@@ -1,11 +1,13 @@
 package com.personal.hotel.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SlotMachineController {
 
+	@PreAuthorize("hasRole('USER')")
 	@GetMapping("/slotmachine")
 	public String slotMachine() {
 		return "slotmachine.html";
