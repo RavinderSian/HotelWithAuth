d28779d4371a77e6ccda015d5fe4bf69d5a64417
ravinder sian <rsian761@gmail.com>
2022-07-17 21:17:31 +0100

    final changes

--- hotel-project-auth/src/main/java/com/personal/hotel/controller/SlotMachineController.java
+++ hotel-project-auth/src/main/java/com/personal/hotel/controller/SlotMachineController.java
@@ -1,7 +1,6 @@
 package com.personal.hotel.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +17,6 @@ public class SlotMachineController {
 	@GetMapping("/slotmachine")
 	public String slotMachine(Model model) {
 		
-		model.addAttribute("discountCode", discountCodeService.generateDiscountCode(40));
 		
 		return "slotmachine.html";
 	}
