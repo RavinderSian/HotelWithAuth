308f510a8af2892b045529d41461c4d9d6469eb5
ravinder sian <rsian761@gmail.com>
2022-07-18 20:17:16 +0100

    winner post mapping

--- hotel-project-auth/src/main/java/com/personal/hotel/controller/SlotMachineController.java
+++ hotel-project-auth/src/main/java/com/personal/hotel/controller/SlotMachineController.java
@@ -1,24 +1,50 @@
 package com.personal.hotel.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.personal.hotel.model.DiscountCode;
 import com.personal.hotel.services.DiscountCodeService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
+@RequestMapping("/slotmachine")
+@PreAuthorize("hasRole('USER')")
 public class SlotMachineController {
 	
 	@Autowired
 	private DiscountCodeService discountCodeService;
 	
-	//@PreAuthorize("hasRole('USER')")
+	@Autowired
+	
+	
 	@GetMapping("/slotmachine")
 	public String slotMachine(Model model) {
 		
 		
+		
 		return "slotmachine.html";
+	}
+	
+	@PostMapping("/winner")
+	public String slotMachineWinner(HttpServletRequest request) {
+		
+		DiscountCode discountCode = discountCodeService.generateDiscountCode(40);
+		
+		String loggedInUser =  request.getUserPrincipal().getName();
+		
+		log.info("User has won: " + loggedInUser);
+		
+		return ":/";
 	}
 	
 }
