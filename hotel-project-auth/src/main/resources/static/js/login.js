2a0ca5e9976b16f5bb82239898bd9f5e70db2aec
ravinder sian <rsian761@gmail.com>
2022-09-06 21:11:43 +0100

    append hidden to login button on successful login

--- hotel-project-auth/src/main/resources/static/js/login.js
+++ hotel-project-auth/src/main/resources/static/js/login.js
@@ -6,7 +6,6 @@ const btnLogin = document.querySelector('.btn-login');
 btnLogin.addEventListener('click', function(e){
 	e.preventDefault();
 	document.querySelector('.login-box').classList.toggle('hidden');
-	console.log(document.querySelector('.login-box').classList);
 
 	btnLogin.element
 	
