371c5591f5e43d8ec03bad33128b15ec9abb376e
ravinder sian <rsian761@gmail.com>
2022-09-10 20:34:27 +0100

    complete login box on home page

--- hotel-project-auth/src/main/resources/static/js/login.js
+++ hotel-project-auth/src/main/resources/static/js/login.js
@@ -6,7 +6,33 @@ const btnLogin = document.querySelector('.btn-login');
 btnLogin.addEventListener('click', function(e){
 	e.preventDefault();
 	document.querySelector('.login-box').classList.toggle('hidden');
+});
 
-	btnLogin.element
-	
+// this is the id of the form
+$(".login-form").submit(function(e) {
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    var form = $(this);
+    var actionUrl = form.attr('action');
+    
+    var xhr = new XMLHttpRequest();
+    
+    $.ajax({
+        type: "POST",
+        url: actionUrl,
+        data: form.serialize(), // serializes the form's elements.
+         xhr: function() {
+         return xhr;
+    	},
+        success: function(){
+			if (!xhr.responseURL.includes('/failedlogin')){
+				document.forms["loginForm"].submit();
+			}
+			else {
+				document.querySelector('.login-error-text').classList.toggle('hidden');
+			}
+		},
+    });
+    
 });
